f3556dd4af2872bd5c36451cb1c0019527082acd
HOLRO <holroyd1214@gmail.com>
2025-02-17 03:59:28 -0800

    Initialize ItemBuilder with basic constructor and item creation

--- src/main/java/com/sandcore/utils/ItemBuilder.java
+++ src/main/java/com/sandcore/utils/ItemBuilder.java
@@ -1,8 +1,18 @@
 package com.sandcore.utils;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 // ... rest of imports ...
 
 public class ItemBuilder {
+    private final ItemStack item;
+    private final ItemMeta meta;
+
+    public ItemBuilder(Material material) {
+        this.item = new ItemStack(material);
+        this.meta = item.getItemMeta();
+    }
+
     // ... class implementation ...
 } 
